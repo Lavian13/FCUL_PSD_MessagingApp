20c9922decb0fda59e1ecbf206d40b0e63893ee3
Guilherme Santos <72032143+guilherme-dsantos@users.noreply.github.com>
2023-11-04 14:59:43 +0000

    TLS/SSL Server-Client comm

--- Client.java
+++ Client.java
@@ -9,6 +9,9 @@ import java.security.spec.*;
 import javax.crypto.*;
 import javax.crypto.spec.*;
 import javax.crypto.interfaces.*;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
 import java.util.Scanner;
 
 // Client class
@@ -17,89 +20,31 @@ class Client {
 
     // driver code
     public static void main(String[] args) throws Exception {
-        // establish a connection by providing host and port
-        // number
 
-        try (Socket socket = new Socket("localhost", 1234)) {
-            OutputStream outputStream = socket.getOutputStream();
-            InputStream inputStream = socket.getInputStream();
-            DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-            DataInputStream dataInputStream = new DataInputStream(inputStream);
+        try {
+            System.setProperty("javax.net.ssl.trustStore", "truststore.p12");
+            System.setProperty("javax.net.ssl.trustStorePassword", "iDcwJk$&TzgJ4NGMn2%M");
 
-            clientDiffieHellman(dataOutputStream, dataInputStream);
+            SSLContext sslContext = SSLContext.getDefault();
+            SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+            SSLSocket sslSocket = (SSLSocket) sslSocketFactory.createSocket("localhost", 1234);
 
+            System.out.println("Connected to server!");
+
+            // Create a PrintWriter to send a message to the server
+            PrintWriter writer = new PrintWriter(sslSocket.getOutputStream(), true);
+            writer.println("Hello, server!");
+
+            // Create a BufferedReader to read the server's messages
+            BufferedReader reader = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
+            String line = reader.readLine();
+            System.out.println("Received from server: " + line);
+
+            sslSocket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
-
-    public static void clientDiffieHellman(DataOutputStream dataOutputStream, DataInputStream dataInputStream) throws Exception {
-        Scanner scanner = new Scanner(System.in);
-        /*
-         * Alice creates her own DH key pair with 2048-bit key size
-         */
-        KeyPairGenerator aliceKpairGen = KeyPairGenerator.getInstance("EC");
-        aliceKpairGen.initialize(521);
-        KeyPair aliceKpair = aliceKpairGen.generateKeyPair();
-
-        // Alice creates and initializes her DH KeyAgreement object
-        KeyAgreement aliceKeyAgree = KeyAgreement.getInstance("ECDH");
-        aliceKeyAgree.init(aliceKpair.getPrivate());
-
-        // Alice encodes her public key, and sends it over to Bob.
-        byte[] alicePubKeyEnc = aliceKpair.getPublic().getEncoded();
-
-        dataOutputStream.writeInt(alicePubKeyEnc.length);
-        dataOutputStream.write(alicePubKeyEnc);
-        System.out.println(Arrays.toString(alicePubKeyEnc));
-
-        /*
-         * Alice uses Bob's public key for the first (and only) phase
-         * of her version of the DH
-         * protocol.
-         * Before she can do so, she has to instantiate a DH public key
-         * from Bob's encoded key material.
-         */
-
-        // Read the length of the array and then the array itself
-        int length = dataInputStream.readInt();
-        byte[] bobPubKeyEnc = new byte[length];
-        dataInputStream.readFully(bobPubKeyEnc);
-
-        KeyFactory aliceKeyFac = KeyFactory.getInstance("EC");
-        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(bobPubKeyEnc);
-        PublicKey bobPubKey = aliceKeyFac.generatePublic(x509KeySpec);
-        aliceKeyAgree.doPhase(bobPubKey, true);
-
-        /*
-         * At this stage, both Alice and Bob have completed the DH key
-         * agreement protocol.
-         * Both generate the (same) shared secret.
-         */
-        byte[] aliceSharedSecret = aliceKeyAgree.generateSecret();
-        int aliceLen = aliceSharedSecret.length;
-        dataOutputStream.writeInt(aliceLen);
-        dataOutputStream.write(aliceSharedSecret);
-        byte[] bobSharedSecret = new byte[aliceLen];
-        dataInputStream.readFully(bobSharedSecret);
-
-        if (!java.util.Arrays.equals(aliceSharedSecret, bobSharedSecret))
-            throw new Exception("Shared secrets differ");
-        System.out.println("Shared secrets are the same");
-
-        System.out.print("Enter a message: ");
-        String userInput = scanner.nextLine();
-        SecretKeySpec aliceAesKey = new SecretKeySpec(aliceSharedSecret, 0, 16, "AES");
-        Cipher bobCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        bobCipher.init(Cipher.ENCRYPT_MODE, aliceAesKey);
-        byte[] cleartext = userInput.getBytes();
-        byte[] ciphertext = bobCipher.doFinal(cleartext);
-        byte[] encodedParams = bobCipher.getParameters().getEncoded();
-        dataOutputStream.writeInt(ciphertext.length);
-        dataOutputStream.write(ciphertext);
-        dataOutputStream.writeInt(encodedParams.length);
-        dataOutputStream.write(encodedParams);
-
-    }
-
 
 }
 
--- Server.java
+++ Server.java
@@ -8,151 +8,45 @@ import java.util.Arrays;
 import javax.crypto.*;
 import javax.crypto.spec.*;
 import javax.crypto.interfaces.*;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLServerSocket;
+import javax.net.ssl.SSLServerSocketFactory;
+import javax.net.ssl.SSLSocket;
 import java.nio.charset.StandardCharsets;
 
 // Server class
 class Server {
 
     public static void main(String[] args) throws Exception {
-        ServerSocket server = null;
 
         try {
+            System.setProperty("javax.net.ssl.keyStore", "keystore.p12");
+            System.setProperty("javax.net.ssl.keyStorePassword", "iDcwJk$&TzgJ4NGMn2%M");
 
+            SSLContext sslContext = SSLContext.getDefault();
+            SSLServerSocketFactory sslServerSocketFactory = sslContext.getServerSocketFactory();
 
-            server = new ServerSocket(1234);
-            server.setReuseAddress(true);
+            SSLServerSocket sslServerSocket = (SSLServerSocket) sslServerSocketFactory.createServerSocket(1234);
 
+            System.out.println("Waiting for client connection...");
+            SSLSocket sslSocket = (SSLSocket) sslServerSocket.accept();
 
-            while (true) {
+            System.out.println("Client connected!");
 
+            // Create a BufferedReader to read the client's messages
+            BufferedReader reader = new BufferedReader(new InputStreamReader(sslSocket.getInputStream()));
+            String line = reader.readLine();
+            System.out.println("Received from client: " + line);
 
-                Socket client = server.accept();
+            // Create a PrintWriter to send a message to the client
+            PrintWriter writer = new PrintWriter(sslSocket.getOutputStream(), true);
+            writer.println("Hello, client!");
 
-                System.out.println("New client connected" + client.getInetAddress().getHostAddress());
-
-
-                ClientHandler clientSock = new ClientHandler(client);
-
-
-                new Thread(clientSock).start();
-            }
-        } catch (IOException e) {
+            sslSocket.close();
+            sslServerSocket.close();
+        } catch (Exception e) {
             e.printStackTrace();
-        } finally {
-            if (server != null) {
-                try {
-                    server.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
         }
     }
 
-    // ClientHandler class
-    private static class ClientHandler implements Runnable {
-        private final Socket clientSocket;
-
-        // Constructor
-        public ClientHandler(Socket socket) {
-            this.clientSocket = socket;
-        }
-
-        public void run() {
-            try {
-                OutputStream outputStream = clientSocket.getOutputStream();
-                InputStream inputStream = clientSocket.getInputStream();
-                DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
-                DataInputStream dataInputStream = new DataInputStream(inputStream);
-
-                serverDH_AES(dataOutputStream, dataInputStream);
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-
-        public static void serverDH_AES(DataOutputStream dataOutputStream, DataInputStream dataInputStream) {
-            try {// Read the length of the array and then the array itself
-                int length = dataInputStream.readInt();
-                byte[] alicePubKeyEnc = new byte[length];
-                dataInputStream.readFully(alicePubKeyEnc);
-
-
-                System.out.println(Arrays.toString(alicePubKeyEnc));
-
-                /*
-                 * Let's turn over to Bob. Bob has received Alice's public key
-                 * in encoded format.
-                 * He instantiates a DH public key from the encoded key material.
-                 */
-                KeyFactory bobKeyFac = KeyFactory.getInstance("EC");
-                X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(alicePubKeyEnc);
-
-                PublicKey alicePubKey = bobKeyFac.generatePublic(x509KeySpec);
-
-                /*
-                 * Bob gets the DH parameters associated with Alice's public key.
-                 * He must use the same parameters when he generates his own key
-                 * pair.
-                 */
-                ECParameterSpec dhParamFromAlicePubKey = ((ECPublicKey) alicePubKey).getParams();
-
-                // Bob creates his own DH key pair
-
-                KeyPairGenerator bobKpairGen = KeyPairGenerator.getInstance("EC");
-                bobKpairGen.initialize(dhParamFromAlicePubKey);
-                KeyPair bobKpair = bobKpairGen.generateKeyPair();
-
-                // Bob creates and initializes his DH KeyAgreement object
-
-                KeyAgreement bobKeyAgree = KeyAgreement.getInstance("ECDH");
-                bobKeyAgree.init(bobKpair.getPrivate());
-
-                // Bob encodes his public key, and sends it over to Alice.
-                byte[] bobPubKeyEnc = bobKpair.getPublic().getEncoded();
-                dataOutputStream.writeInt(bobPubKeyEnc.length);
-                dataOutputStream.write(bobPubKeyEnc);
-
-
-                bobKeyAgree.doPhase(alicePubKey, true);
-
-                // Read the length of the array and then the array itself
-                length = dataInputStream.readInt();
-                byte[] aliceSharedSecret = new byte[length];
-                dataInputStream.readFully(aliceSharedSecret);
-
-                byte[] bobSharedSecret = new byte[length];
-                bobKeyAgree.generateSecret(bobSharedSecret, 0);
-
-                if (!java.util.Arrays.equals(aliceSharedSecret, bobSharedSecret))
-                    throw new Exception("Shared secrets differ");
-                System.out.println("Shared secrets are the same");
-
-                dataOutputStream.write(bobSharedSecret);
-
-                SecretKeySpec bobAesKey = new SecretKeySpec(bobSharedSecret, 0, 16, "AES");
-                length = dataInputStream.readInt();
-                byte[] ciphertext = new byte[length];
-                dataInputStream.readFully(ciphertext);
-                String encryptedMessage = new String(ciphertext, StandardCharsets.UTF_8);
-                System.out.println("Mensagem cifrada: " + encryptedMessage);
-
-                length = dataInputStream.readInt();
-                byte[] encodedParams = new byte[length];
-                dataInputStream.readFully(encodedParams);
-
-                AlgorithmParameters aesParams = AlgorithmParameters.getInstance("AES");
-                aesParams.init(encodedParams);
-                Cipher aliceCipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-                aliceCipher.init(Cipher.DECRYPT_MODE, bobAesKey, aesParams);
-                byte[] recovered = aliceCipher.doFinal(ciphertext);
-                String decryptedMessage = new String(recovered, StandardCharsets.UTF_8);
-                System.out.println("Mensagem decifrada: " + decryptedMessage);
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
 }
