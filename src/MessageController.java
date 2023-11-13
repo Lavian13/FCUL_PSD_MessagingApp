50fee0e66366d3e3eade0dbcf5975eed2ad681c0
Lavian13 <lcava@iscte-iul.pt>
2023-11-13 14:30:35 +0000

    Merge with TLS/SSL branch

---
+++ src/MessageController.java
@@ -0,0 +1,15 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+
+public class MessageController {
+
+
+
+    @FXML
+    public void initialize() throws IOException {
+
+    }
+
+}
